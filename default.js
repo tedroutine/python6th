557dc5f1922feff508574d10e31e2537063d6e3c
Ted Lee <taeheon7753@gmail.com>
2023-05-30 13:15:23 +0900

    add variables

--- default.js
+++ default.js
@@ -1,1 +1,36 @@
-document.write("hello world!");
+// document.write("hello world!");
+// document.write("<h1>Welcome to JS</h1>");
+
+// console.log("welcome to JS")
+// console.warn("welcome to JS")
+// console.error("welcome to JS")
+
+// alert("welcome to JS")
+
+// console.log(typeof 123);
+// console.log(typeof 123.5);
+// console.log(typeof "123");
+// console.log(typeof true);
+// console.log(typeof -1);
+
+// var car
+// var car = ""
+// var person = {firstName: "John", lastName: "Lee", age: 50, eyeColor: "blue"}
+// console.log(person)
+// console.log(typeof person)
+// person = null
+// console.log(person)
+// console.log(typeof person)
+
+// person1 = null
+// console.log(person1)
+// console.log(typeof person1)
+
+var name = "Ted Lee";
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br/>"
+
+document.write("name is " + name + lineBreak);
+document.write("age is " + age + lineBreak);
+
