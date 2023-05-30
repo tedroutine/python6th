3ecfa6587b1d2f1260a466a6c80651f6c61d4ce1
Ted Lee <taeheon7753@gmail.com>
2023-05-30 13:57:22 +0900

    JS study

--- default.js
+++ default.js
@@ -26,11 +26,43 @@
 // console.log(person1)
 // console.log(typeof person1)
 
-var name = "Ted Lee";
-var age = 29;
-var cgpa = 3.92;
-var lineBreak = "<br/>"
+// var name = "Ted Lee";
+// var age = 29;
+// var cgpa = 3.92;
+// var lineBreak = "<br/>"
 
-document.write("name is " + name + lineBreak);
-document.write("age is " + age + lineBreak);
+// document.write("name is " + name + lineBreak);
+// document.write("age is " + age + lineBreak);
 
+// var lastName = "Lee";
+// var firstName = "Ted";
+
+// var fullName =  firstName + " " + lastName;
+// console.log(fullName)
+
+// var num1 = 20
+// var num2 = 30
+// var sum = num1 + num2
+// console.log(sum)
+
+// var text = prompt("Enter Your Name");
+// document.write("Your Name : " + text + "<br/>");
+
+// var len = text.length
+// document.write("Number of characters : " + len + "<br/>");
+
+// document.write(text.charAt(2) + "<br/>")
+
+// document.write(text.toUpperCase() + "<br/>")
+// document.write(text.toLowerCase() + "<br/>")
+
+var text1 = " hi"
+var text2 = "bye"
+var text3 = text1.concat(text2);
+document.write(text3 + "<br/>")
+
+var text4 = "hello";
+var result = text4.slice(0,2)
+document.write(result + "<br/>")
+var result2 = text4.split('e')
+document.write(result2 + "<br/>")
