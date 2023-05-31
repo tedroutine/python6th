f1fe05dedf16bfaaa66c83b7ec47ef2463a7ae61
Ted Lee <taeheon7753@gmail.com>
2023-05-31 09:48:30 +0900

    조건부 제어문

--- default.js
+++ default.js
@@ -114,36 +114,62 @@
 // var farn = cels * (9 / 5) + 32
 // document.write("화씨 : " + farn)
 
-var num1 = 20;
-var num2 = 10;
-var num3 = "10";
-var num4 = 20;
-var num5 = 15;
+// var num1 = 20;
+// var num2 = 10;
+// var num3 = "10";
+// var num4 = 20;
+// var num5 = 15;
 
-console.log('일반크기 비교')
-console.log(num1 > num2, num1, '>', num2 )
-console.log(num1 >= num2, num1, '>=', num2 )
-console.log(num1 < num2, num1, '<', num2 )
-console.log(num1 <= num2, num1, '<=', num2 )
+// console.log('일반크기 비교')
+// console.log(num1 > num2, num1, '>', num2 )
+// console.log(num1 >= num2, num1, '>=', num2 )
+// console.log(num1 < num2, num1, '<', num2 )
+// console.log(num1 <= num2, num1, '<=', num2 )
 
-console.log('같은지 여부 비교')
-console.log(num1 == num4, num1, '==', num4 )
-console.log(num1 != num4, num1, '!=', num4 )
+// console.log('같은지 여부 비교')
+// console.log(num1 == num4, num1, '==', num4 )
+// console.log(num1 != num4, num1, '!=', num4 )
 
-console.log('=== 비교 (타입)')
-console.log(num1 === num3, num1, '===', num3 )
-console.log(num2 === num3, num2, '===', num3 )
-console.log(num2 == num3, num2, '==', num3 )
+// console.log('=== 비교 (타입)')
+// console.log(num1 === num3, num1, '===', num3 )
+// console.log(num2 === num3, num2, '===', num3 )
+// console.log(num2 == num3, num2, '==', num3 )
 
-console.log('and(&&) or 연산자(||)')
-console.log('num1 > num2 && num1 >num5 : ', num1 > num2 && num1 >num5)
-console.log('num1 > num2 || num2 >num5 : ', num1 > num2 || num2 >num5)
-
-
+// console.log('and(&&) or 연산자(||)')
+// console.log('num1 > num2 && num1 >num5 : ', num1 > num2 && num1 >num5)
+// console.log('num1 > num2 || num2 >num5 : ', num1 > num2 || num2 >num5)
 
 
+var num1 = parseInt(prompt('첫 번째 숫자 입력 : '))
+var num2 = parseInt(prompt('두 번째 숫자 입력 : '))
 
+if (num1 > num2) {
+    console.log('큰 수는 : num1 ' + num1)
+}
+    
+if (num1 < num2) {
+    console.log('큰 수는 : num2 ' + num2)
+}
 
+if (num1 == num2) {
+    console.log('같은 수')
+}
+
+if (num1 > num2) {
+    console.log('큰 수는 : num1 ' + num1)
+} else if (num1 < num2) {
+    console.log('큰 수는 : num2 ' + num2)
+} else if (num1 == num2) {
+    console.log('같은 수')
+}
+
+if (num1 > num2) {
+    console.log('큰 수는 : num1 ' + num1)
+} else if (num1 < num2) {
+    console.log('큰 수는 : num2 ' + num2)
+} else {
+    console.log('같은 수')
+}
 
 
 
