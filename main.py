844d21265f692cc8a63e2faaf938fe142955bd90
Ted Lee <taeheon7753@gmail.com>
2023-06-05 10:22:43 +0900

    pythong variables study

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World!')
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = [1, 2, 3]
+e = (4, 5, 6)
+f = {7, 8, 9, 7}
+g = {"Apple": 1, "banana":2, "orange":3}
+
+print("a: ", type(a))
+print("b: ",type(b))
+print("c: ",type(c))
+print("d: ",type(d))
+print("e: ",type(e))
+print("f: ",type(f))
+print("g: ",type(g))
+ 
