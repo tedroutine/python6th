1f2084f0e05d9d06ec3227b428545bf8007c0209
Ted Lee <taeheon7753@gmail.com>
2023-06-07 19:27:17 +0900

    python function - function(function)

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ def display_info(**kwargs):
     for key, value in kwargs.items():
         print(f"{key}:{value}")
 
-display_info(name="tes", age=25)
+# display_info(name="tes", age=25)
 
+
+def disp(sh):
+    print(type(sh))
+    print("Disp Function" + sh())
+
+def show():
+    return " Show Function"
+
+disp(show)
