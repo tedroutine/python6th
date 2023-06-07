0de078689723302832dfd3db03252d266a4514ab
Ted Lee <taeheon7753@gmail.com>
2023-06-07 11:40:49 +0900

    python array

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-# tuple
+from array import *
+# import array
 
-a = "멋쟁이 사자"
-b = tuple(a)
+stu_roll = array('i', [101, 102, 103, 104, 105])
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
 
-print(b, type(b))
+print("Array After Insert")
+
+# insert
+stu_roll.insert(1, 106)
+stu_roll.insert(3, 107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print("Array After Remove")
+
+# remove
+stu_roll.remove(107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print("Array After Pop")
+
+# pop
+stu_roll.pop()
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print("Array After Pop(n)") # n번째 요소 삭제
+# pop
+stu_roll.pop(2)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
