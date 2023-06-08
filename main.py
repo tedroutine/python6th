f7571975a6d9619eac960b9e83e0393377673a63
Ted Lee <taeheon7753@gmail.com>
2023-06-08 15:29:51 +0900

    python function Call by Value

--- main.py
+++ main.py
@@ -1,49 +1,20 @@
-# dictionary
-stu = {101: 'Kim', 102: 'Bae', 103: 'Hong'}
-fees = {'kim': 2000, 'bae':3000, 'hong':8000}
-print(stu[101])
-print(stu[102])
-print(stu[103])
+# def val(lst):
+    print("inside Function Before Append", lst, id(list))
+    lst.append(4)
+    print("inside Function Before Append", lst, id(list))
 
-print(fees['kim'])
-print(fees['bae'])
-print(fees['hong'])
-
-stu[102] = 'Python'
-print(stu[102])
-
-stu[104] = 'Like Lion'
-print(stu)
-
-del stu[102]
-print(stu)
-
-# stu.clear()
-# print(stu)
-
-key = (101, 102, 103)
-value = 'like lion'
-new_stu = dict.fromkeys(key, value)
-print(new_stu)
-
-print(stu.items())
-print(stu.keys())
-print(stu.values(), type(stu.values()))
-
-stu.update({102: 'Lee'})
-print(stu)
-
-# dict에서 102(key 값)으로 제거 가능
-stu.pop(102)
-print(stu)
-
-print(stu.popitem())
-print(stu)
-print(stu.popitem())
-print(stu)
-print(stu.popitem())
-print(stu)
+list = [1, 2, 3]
+print("Before Calling Function: ", list, id(list))
+val(list)
+print("After Calling Function: ", list, id(list))
 
 
+def val2(x):
+    print("Inside : ", id(x))
+    x += 1
+    print("Inside After : ", id(x))
 
-
+x = 10
+print("Before Calling Function: ", x, id(x))
+val2(x)
+print("After Calling Function: ", x, id(x))
