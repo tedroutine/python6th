86ef2c4395f04bfa2779a8f5e077bab1f74f0cee
Ted Lee <taeheon7753@gmail.com>
2023-06-08 10:44:13 +0900

    python function - Local variables > Global variables

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def add(**num):  # num: {'a':5, 'b':2, 'c':1. 'd':5}
-    z = num['a'] + num['b'] + num['c']
-    print('Addition: ', z)
+a = 50
 
 
-add(a=5, b=5, c=10)
+def show():
+    x = 10
+    print(x)
+    print(a)
+
+
+show()
+
+print("global a : ", a)
+
+i = 0
+
+
+def myfun():
+    a = i + 1
+    print("My Function", a)  # read local variables first
+
+
+myfun()
+print("global a : ", a)
+
+i = 0
+
+
+def myfun2():
+    t = 0
+    t = t + 1  # global 변수와 local 변수가 같아서 오류가 생긴다. global 변수를 read할 수 있지만 로컬과 같았을 때는 충돌
+    print(t)
+
+
+myfun2()
