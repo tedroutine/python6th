0ab67e99f66c3f5d78436ed1f55ae400694e8e03
Ted Lee <taeheon7753@gmail.com>
2023-06-09 13:37:32 +0900

    python file

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-class ParentClass:
-    def __init__(self):
-        self.name = 'parent'
-        self.number = 10
+# read the file
 
-    def __str__(self):
-        return f'ParentClass name : {self.name}, number : {self.number}'
-
-    def add_num(self, new_number):
-        print('부모 : ', new_number, '만큼 더해야지')
-        self.number += new_number
-
-
-class ChildClass(ParentClass):
-    def __init__(self):
-        super().__init__()  # 부모 class 바로 가져와서 그대로 받아들임. __init__ 함수 빼고 pass와 같음
-        self.name = 'child'
-
-    def __str__(self):
-        return f'ChildClass name : {self.name}, number : {self.number}'
-
-    def add_num(self, new_number):
-        print('말 안듣는 자식: 고정적으로 5를 더할건데?')
-        self.number += 5
-
-
-parent = ParentClass()
-child = ChildClass()
-print(parent)
-print(child)
-print('---------------')
-
-print('7일을 더하세요.')
-parent.add_num(7)
-child.add_num(7)
-
-print(parent)
-print(child)
+with open('new_example.txt', 'w') as file_object:
+    content = 'new exmaple'
+    file_object.write(content)
