3e4381fcdd28414cf147e451f9578ec7e3c7a504
Ted Lee <taeheon7753@gmail.com>
2023-06-09 14:19:50 +0900

    python file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# read the file
+import os
 
-with open('new_example.txt', 'w') as file_object:
-    content = 'new exmaple'
-    file_object.write(content)
+filename = 'example1.txt'
+
+print("파일이 존재하는지 확인하기")
+if os.path.isfile(filename):
+    print(f"{filename}이 존재합니다.")
+else:
+    print(f"{filename}이 없습니다.")
+
+file_object = open("list_example.txt", 'w')
+content_list = ["python", "Java", "C++", "Javascript"]
+
+for item in content_list:
+    file_object.write(item + '\n')
+
+file_object.close()
