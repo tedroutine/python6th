3b8d1cafb37ff192e54866b003580c0e39fbe608
Ted Lee <taeheon7753@gmail.com>
2023-06-09 10:33:35 +0900

    python class super - sub

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-# class
+# class 2
 
-class Monster:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class Quote:
+    def __init__(self, person, words):
+        self.person = person
+        self.words = words
+
+    def who(self):
+        return self.person
 
     def say(self):
-        print(f"I am {self.name} and my age is {self.age}")
+        return self.words + '.'
 
 
-shark = Monster('shark', 7)
-wolf = Monster('wolf', 3)
-shark.say()
+class Quote1(Quote):
+    def say(self):
+        return self.words + '!'
+
+
+class Quote2(Quote):
+    def say(self):
+        return self.words + '?'
+
+
+quote = Quote("person1", "what's up1")
+quote1 = Quote1("person2", "what's up2")
+quote2 = Quote2("person3", "what's up3")
+
+a = quote
+b = quote1
+c = quote2
+
+print(a.who(), a.say())
+print(b.who(), b.say())
+print(c.who(), c.say())
