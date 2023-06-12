ab3b6ba9dc6d158eace5712aceba06aa914f4017
Ted Lee <taeheon7753@gmail.com>
2023-06-12 13:43:12 +0900

    python unpacking

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-# 예외가 하나인 경우
-def devide(x, y):
-    try:
-        result = x / y
-        print(f"x / y = {result}")
-    except:
-        print("Error!")
-    finally:
-        print("This is Math!")
-
-devide(10, 2)
-
-# 예외가 하나 이상인 경우
-try:
-    number = 5 + int("not a number")
-except ValueError:
-    print("error - invalid number")
-except TypeError:
-    print("error - invalid type")
+# 언패킹 : *
+# 리스트 앞에 * 있으면 값 하나씩 감싸는 함수 프로퍼티 값에 추가된다.
 
 
-# custom exception
-
-class CustomException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-try:
-    raise CustomException("This is a custom exception.")
-except CustomException as e:
-    print(f"Error: {e.message}")
+def add(x, y, z):
+    return x + y + z
 
 
+n = [1, 2, 3]
+print(add(*n))
