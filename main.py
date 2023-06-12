79fa572282d81cadd2f73d5c24f7da510112d92b
Ted Lee <taeheon7753@gmail.com>
2023-06-12 13:46:58 +0900

    python lambda & list comprehension

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# 언패킹 : *
-# 리스트 앞에 * 있으면 값 하나씩 감싸는 함수 프로퍼티 값에 추가된다.
+# lambda & list comprehension
 
+print(list(map(lambda x: x + 10, [1, 2, 3])))
 
-def add(x, y, z):
-    return x + y + z
-
-
-n = [1, 2, 3]
-print(add(*n))
+print([n + 10 for n in range(1,11) if n % 2 == 1])
