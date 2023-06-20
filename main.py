aa73e91ae5c0bcf8b961056bf6e2478bdb3aee24
Ted Lee <taeheon7753@gmail.com>
2023-06-20 14:15:47 +0900

    django project initiation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import pprint
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-pprint.pprint(locals())
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200, 'OK')
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello World")
+
+
+server = HTTPServer(("", 8080), SimpleHTTPRequestHandler)
+server.serve_forever()
