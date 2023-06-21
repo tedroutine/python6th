30b8a007488a9fae789b7186d51548c373c5132f
Ted Lee <taeheon7753@gmail.com>
2023-06-21 15:39:51 +0900

    django result function

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -32,6 +32,6 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
-
 def results(request, question_id):
-    return HttpResponseRedirect(reverse("polls:index"))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
