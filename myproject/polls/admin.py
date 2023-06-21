1051e66137a4c1d590ff6c6b25258e5942125c77
Ted Lee <taeheon7753@gmail.com>
2023-06-21 13:25:13 +0900

    django 전체 프로젝트 urlconfig 하는 케이스

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-# Register your models here.
+# Register your models here. 테이블에 CRUD를 할 수 있게 하는 명령어
 admin.site.register(Question)
 admin.site.register(Choice)
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    pass
+
+
+def detail(request):
+    pass
+
+
+def vote(request):
+    pass
+
+
+def results(request):
+    pass
