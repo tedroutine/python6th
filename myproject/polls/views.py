f3bccadb2ceec9c85aa81bb946e16d1c1157e1ae
Ted Lee <taeheon7753@gmail.com>
2023-06-22 14:02:11 +0900

    django 템플릿 상속

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -7,9 +7,8 @@ from polls.models import Question, Choice
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    context = {'latest_question_list': latest_question_list, 'value': '<strong>Hello, World</strong>'}
     return render(request, 'polls/index.html', context)
-
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
