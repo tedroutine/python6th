bc716187abb93d2f81f463543c2d644aae56c47c
Ted Lee <taeheon7753@gmail.com>
2023-06-23 15:22:12 +0900

    django add polls template

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -10,26 +10,24 @@ from polls.models import Question, Choice
 logger = logging.getLogger(__name__)
 
 
+# Create your views here.
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+
     def get_queryset(self):
         # 최근 생성된 질문 3개를 반환하도록 설계
-        return Question.objects.order_by('-pub_date')[:3]
+        return Question.objects.order_by('-pub_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list, 'value': '<strong>Hello, World</strong>'}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -47,8 +45,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
